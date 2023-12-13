49909594643bdbf707db7203b008ec6fce096051
Nicolas RAYNAUD <contact@nicolas-raynaud.fr>
2023-12-13 13:11:27 +0100

    Improvements and updated datas

--- back-end/migrations/1679933977703-create_users_table.ts
+++ back-end/migrations/1679933977703-create_users_table.ts
@@ -83,10 +83,10 @@ export class createUsersTable1679933977703 implements MigrationInterface {
 
             await queryRunner.query(`INSERT INTO player (id, name, age, nationality, poste, image) 
             VALUES 
-              (1, 'Mbappe', 25, 'Français', 'BU', 'https://futhead.cursecdn.com/static/img/23/players/231747.png'),
-              (2, 'Messi', 34, 'Argentin', 'AD', 'https://futhead.cursecdn.com/static/img/23/players/158023.png'),
-              (3, 'Neymar', 31, 'Brésilien', 'AG', 'https://futhead.cursecdn.com/static/img/23/players/190871.png'),
-              (4, 'Ronaldo', 38, 'Portugais', 'AG', 'https://futhead.cursecdn.com/static/img/23/players/20801.png');`);
+              (1, 'Nicolas', 20, 'Français', 'ShooterMan', 'https://images-ext-1.discordapp.net/external/a1NR9pgQjwdcDQ2u7ll9m7WZYFN6fymz5nxFDtr6pto/%3Fw%3D270%26h%3D270%26c%3D6%26r%3D0%26o%3D5%26pid%3DImgGn/https/th.bing.com/th/id/OIG.sDmOQhjJCpjC9kQ_2hoN?format=webp'),
+              (2, 'Guillaume', 34, 'Français', 'Oenologue', 'https://th.bing.com/th/id/OIG.N_Ak9ZCgiIM4gwJJbYip?w=270&h=270&c=6&r=0&o=5&pid=ImgGn'),
+              (3, 'Lorène', 26, 'Française', 'Poche', 'https://media.discordapp.net/attachments/1070450741052973056/1184466099077660713/6522c8a8-5245-4bc1-a3f9-00d6800c2ca7.jpg?ex=658c1304&is=65799e04&hm=8871da5134e787730baca583b55abbf09656beef4499b5325c0ce2dcbd508799&=&format=webp&width=463&height=463'),
+              (4, 'Romain', 23, 'Asiatique', 'Pillier de Bar', 'https://images-ext-1.discordapp.net/external/xzJJP9ac82IYijGSeKz_meuAwWMQk6PYvBQIr2xMB9M/%3Fw%3D270%26h%3D270%26c%3D6%26r%3D0%26o%3D5%26pid%3DImgGn/https/th.bing.com/th/id/OIG.5LD6Qr_UwAVEx.TeyMcO?format=webp');`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {

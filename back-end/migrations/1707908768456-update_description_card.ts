061c8fa94de563f5ee89315cf0a11eaaa540dab2
romain-pons <romain.pons26@gmail.com>
2024-02-14 13:01:55 +0100

    Fix synthax

--- back-end/migrations/1707908768456-update_description_card.ts
+++ back-end/migrations/1707908768456-update_description_card.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class updateDescriptionCard1707908768456 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`UPDATE player SET description = 'Adore le rhum' WHERE id = 1`);
-        await queryRunner.query(`UPDATE player SET description = 'Pense encore que le vin blanc est de l\'eau' WHERE id = 2`);
-        await queryRunner.query(`UPDATE player SET description = 'Une journée sans alcool est une journée ratée' WHERE id = 3`);
-        await queryRunner.query(`UPDATE player SET description = 'Pense que l\'alcool c\'est cool car il y a le mot cool dans alcool' WHERE id = 4`);
+        await queryRunner.query(`UPDATE player SET description = "Adore le rhum" WHERE id = 1`);
+        await queryRunner.query(`UPDATE player SET description = "Pense encore que le vin blanc est de l'eau" WHERE id = 2`);
+        await queryRunner.query(`UPDATE player SET description = "Une journée sans alcool est une journée ratée" WHERE id = 3`);
+        await queryRunner.query(`UPDATE player SET description = "Pense que l'alcool c'est cool car il y a le mot cool dans alcool" WHERE id = 4`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
